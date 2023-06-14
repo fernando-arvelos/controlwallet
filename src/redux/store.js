1f6484d33864085f98142f4936921c4b2e3dbc9d
Fernando Arvelos <fernandoarvelos1@gmail.com>
2023-06-14 14:58:21 -0300

    Iniciando o projeto

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,6 @@
-// configure aqui sua store
+import { legacy_createStore as createStore } from 'redux';
+import { rootReducer } from './reducer';
+
+const store = createStore(rootReducer);
+
+export default store;
