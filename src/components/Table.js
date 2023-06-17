5fa75f83762e177895e8975f1939eb0a5744199f
Fernando Arvelos <fernandoarvelos1@gmail.com>
2023-06-17 11:37:00 -0300

    feat(6) Desenvolva uma tabela com os gastos contendo as seguintes características

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,7 +3,19 @@ import React, { Component } from 'react';
 class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <div>
+        <tr>
+          <th scope="row">Descrição</th>
+          <th scope="row">Tag</th>
+          <th scope="row">Método de pagamento</th>
+          <th scope="row">Valor</th>
+          <th scope="row">Moeda</th>
+          <th scope="row">Câmbio utilizado</th>
+          <th scope="row">Valor convertido</th>
+          <th scope="row">Moeda de conversão</th>
+          <th scope="row">Editar/Excluir</th>
+        </tr>
+      </div>
     );
   }
 }
